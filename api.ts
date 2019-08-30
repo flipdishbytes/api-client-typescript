ace840e060af169faef703e9ef76d19008586850
Ubuntu <flipdishadmin@UbuntuBuildAgents.zbknkd0jxsnelf1lqgn3sborod.parx.internal.cloudapp.net>
2019-08-30 13:31:03 +0000

    Auto generated

--- api.ts
+++ api.ts
@@ -663,13 +663,9 @@ export class App {
     */
     'CountryId'?: string;
     /**
-    * Icon url (Application Icon \\ Logo)
+    * Application Logo
     */
-    'IconUrl'?: string;
-    /**
-    * Icon thumbnail url (Application Icon \\ Logo thumbnail)
-    */
-    'IconThumbnailUrl'?: string;
+    'LogoImageUrl'?: string;
     /**
     * Languages to be available in the application
     */
@@ -747,13 +743,8 @@ export class App {
             "type": "string"
         },
         {
-            "name": "IconUrl",
-            "baseName": "IconUrl",
-            "type": "string"
-        },
-        {
-            "name": "IconThumbnailUrl",
-            "baseName": "IconThumbnailUrl",
+            "name": "LogoImageUrl",
+            "baseName": "LogoImageUrl",
             "type": "string"
         },
         {
@@ -28188,7 +28179,7 @@ export class HeartbeatApi {
      * @param {*} [options] Override http request options.
      */
     public heartbeatHostname (options: any = {}) : Promise<{ response: http.IncomingMessage; body: any;  }> {
-        const localVarPath = this.basePath + '/api/{apiVersion}/Heartbeat/Hostname';
+        const localVarPath = this.basePath + '/api/v1.0/heartbeat/Hostname';
         let localVarQueryParameters: any = {};
         let localVarHeaderParams: any = (<any>Object).assign({}, this.defaultHeaders);
         let localVarFormParams: any = {};
@@ -28198,7 +28189,56 @@ export class HeartbeatApi {
         let localVarUseFormData = false;
 
         let localVarRequestOptions: localVarRequest.Options = {
-            method: 'POST',
+            method: 'GET',
+            qs: localVarQueryParameters,
+            headers: localVarHeaderParams,
+            uri: localVarPath,
+            useQuerystring: this._useQuerystring,
+            json: true,
+        };
+
+        this.authentications.oauth2.applyToRequest(localVarRequestOptions);
+
+        this.authentications.default.applyToRequest(localVarRequestOptions);
+
+        if (Object.keys(localVarFormParams).length) {
+            if (localVarUseFormData) {
+                (<any>localVarRequestOptions).formData = localVarFormParams;
+            } else {
+                localVarRequestOptions.form = localVarFormParams;
+            }
+        }
+        return new Promise<{ response: http.IncomingMessage; body: any;  }>((resolve, reject) => {
+            localVarRequest(localVarRequestOptions, (error, response, body) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    body = ObjectSerializer.deserialize(body, "any");
+                    if (response.statusCode && response.statusCode >= 200 && response.statusCode <= 299) {
+                        resolve({ response: response, body: body });
+                    } else {
+                        reject({ response: response, body: body });
+                    }
+                }
+            });
+        });
+    }
+    /**
+     * 
+     * @param {*} [options] Override http request options.
+     */
+    public heartbeatHostname_1 (options: any = {}) : Promise<{ response: http.IncomingMessage; body: any;  }> {
+        const localVarPath = this.basePath + '/api/v1.0/heartbeat/Hostname';
+        let localVarQueryParameters: any = {};
+        let localVarHeaderParams: any = (<any>Object).assign({}, this.defaultHeaders);
+        let localVarFormParams: any = {};
+
+        (<any>Object).assign(localVarHeaderParams, options.headers);
+
+        let localVarUseFormData = false;
+
+        let localVarRequestOptions: localVarRequest.Options = {
+            method: 'HEAD',
             qs: localVarQueryParameters,
             headers: localVarHeaderParams,
             uri: localVarPath,
@@ -28285,7 +28325,7 @@ export class HeartbeatApi {
      * 
      * @param {*} [options] Override http request options.
      */
-    public ping_1 (options: any = {}) : Promise<{ response: http.IncomingMessage; body: RestApiStringResult;  }> {
+    public ping_2 (options: any = {}) : Promise<{ response: http.IncomingMessage; body: RestApiStringResult;  }> {
         const localVarPath = this.basePath + '/api/v1.0/heartbeat';
         let localVarQueryParameters: any = {};
         let localVarHeaderParams: any = (<any>Object).assign({}, this.defaultHeaders);
