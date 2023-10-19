a8248ef3ef33c2b34c5b1f4d4077afad6dcced26
Azure DevOps <noreply@flipdish.com>
2023-10-19 07:21:52 +0000

    Auto generated

--- api.ts
+++ api.ts
@@ -31170,6 +31170,10 @@ export class PercentDiscountDetails {
     * Percentage discount
     */
     'PercentageDiscount'?: number;
+    /**
+    * The maximum amount that the percentage discount value can be.  If not set then there is no maximum.  If set to e.g. 20 euro, and the percentage discount is 25%, and the order total is 100 euro, then the discount will be 20 euro (not 25 euro).
+    */
+    'MaxDiscountAmount'?: number;
 
     static discriminator: string | undefined = undefined;
 
@@ -31177,6 +31181,11 @@ export class PercentDiscountDetails {
         {
             "name": "PercentageDiscount",
             "baseName": "PercentageDiscount",
+            "type": "number"
+        },
+        {
+            "name": "MaxDiscountAmount",
+            "baseName": "MaxDiscountAmount",
             "type": "number"
         }    ];
 
